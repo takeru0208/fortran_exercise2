5de3e933f1c9c752da3b6223e9206bba5a9a73a9
Takeru Tsuchiya <aikonntatto@gmail.com>
2020-10-25 20:11:20 +0900

    ヒープソートを導入, exercise1完成

--- exercise1.f95
+++ exercise1.f95
@@ -1,7 +1,6 @@
 program exercise1
   implicit none
 
-
   integer :: i, j
   integer(8) :: data(5, 50), outdata(6, 50)
   read(*,*) data
@@ -28,17 +27,82 @@ program exercise1
     write(17, *) ''
   end do
   close(17)
-  call hello('Meichel')
+  call heapsort(50, outdata)
+
+  open(17, file='sorted_ex1.dat', status='replace') 
+  do i = 1, 50
+    do j = 1, 6
+      write(17, fmt='(i3)', advance='no') outdata(j, i)
+      if(j >=6) then
+        exit
+      end if
+      write(17, fmt='(a)', advance='no') ', '
+    end do
+    write(17, *) ''
+  end do
+  close(17)
   stop
 
-contains
-  subroutine hello(name)          ! helloという名前でサブルーチンを宣言
-    implicit none                 ! 暗黙の型宣言の禁止
-    character(len=*) :: name      ! 引数を宣言 (任意長の文字列)
 
-    write(*,*) 'Hello ', name     ! 内部の処理
+! ヒープソートを行う
+contains
+  subroutine heapsort(n,array)
+    implicit none
+    integer,intent(in) :: n
+    integer(8),intent(inout) :: array(6, n)
+  
+    integer ::i,k,j,l, p
+    integer(8) :: t(6)
+  
+    if(n.le.0)then
+      write(6,*)"Error, at heapsort"; stop
+    endif
+    if(n.eq.1)return
+
+    l=n/2+1
+    k=n
+    do while(k.ne.1)
+      if(l.gt.1)then
+          l=l-1
+          do p = 1, 6 
+            t(p)=array(p, L)
+          end do
+      else
+        do p = 1, 6
+          t(p)=array(p, k)
+          array(p, k)=array(p, 1)
+        end do 
+        
+        k=k-1
+        if(k.eq.1) then
+          do p = 1, 6
+            array(p, 1)=t(p)
+          end do
+          exit
+        endif
+      end if
+      i=l
+      j=l+l
+      do while(j.le.k)
+          if(j.lt.k)then
+            if(array(6, j).lt.array(6, j+1))j=j+1
+          end if
+          if (t(6).lt.array(6, j))then
+            do p= 1, 6
+              array(p, i)=array(p, j)
+            end do
+            i=j
+            j=j+j
+          else
+            j=k+1
+          end if
+      end do
+      do p = 1, 6
+        array(p, i)=t(p)
+      end do
+    enddo
 
     return
-  end subroutine hello
+  end subroutine heapsort
 
 end program exercise1
